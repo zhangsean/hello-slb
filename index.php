0f9b765b9016cf38840ce2aeef51f384cef8c7b2
Sean Zhang <zxf2342@qq.com>
2018-03-21 20:23:49 +0800

    Client real IP

--- index.php
+++ index.php
@@ -9,8 +9,23 @@ echo "Server Port: ".$_SERVER['SERVER_PORT']."<br/>";
 <hr/>
 <h3>Current Client Request Info</h3>
 <?php
+function getClientIP() {
+    $keys = array(
+        'HTTP_CLIENT_IP',
+        'HTTP_X_REAL_IP',
+        'HTTP_X_FORWARDED_FOR',
+        'REMOTE_ADDR'
+    );
+    foreach ($keys as $key) {
+        $ip = !empty($_SERVER[$key]) ? $_SERVER[$key] : getenv($key);
+        if (!empty($ip)) {
+            return $ip;
+        }
+    }
+    return '';
+}
 echo "Request Time Float: ".$_SERVER['REQUEST_TIME_FLOAT']."<br/>";
-echo "Client IP: ".$_SERVER['REMOTE_ADDR']."<br/>";
+echo "Client IP: ".getClientIP()."<br/>";
 echo "Client Port: ".$_SERVER['REMOTE_PORT']."<br/>";
 echo "User Agent: ".$_SERVER['HTTP_USER_AGENT']."<br/>";
 echo "Request Method: ".$_SERVER['REQUEST_METHOD']."<br/>";
